20c8df99272a672fdb8f2a78591bee73ea9964b8
Shubham kahar <Shubhamkahar196@gmail.com>
2025-12-30 09:13:37 +0530

    added signout logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,33 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { removeUser } from "../utils/useSlice";
 
 const Header = () => {
+  const navigate = useNavigate();
+ 
+
+  const dispatch = useDispatch();
+  const user = useSelector((store)=> store.user)
+
+  const handleSignIn = () => {
+    navigate("/");
+  };
+
+  
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        dispatch(removeUser());
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error("Sign out error:", error);
+      });
+  };
+
   return (
     <div className="relative ">
       <img
@@ -8,9 +35,21 @@ const Header = () => {
         src="https://images.ctfassets.net/y2ske730sjqp/6bhPChRFLRxc17sR8jgKbe/6fa1c6e6f37acdc97ff635cf16ba6fb3/Logos-Readability-Netflix-logo.png"
         alt="logo"
       />
-      <button className="absolute top-0 right-0 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded m-2 mt-12">
-        Sign In
-      </button>
+      {user ? (
+        <button
+          onClick={handleSignOut}
+          className="absolute top-0 right-0 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded m-2 mt-12"
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button
+          onClick={handleSignIn}
+          className="absolute top-0 right-0 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded m-2 mt-12"
+        >
+          Sign In
+        </button>
+      )}
     </div>
   );
 };
